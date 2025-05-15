f30cb3e7abfc483ab4ba04505bc6b4de34bc081e
ibukun-oyedeji <awesomeibukun@gmail.com>
2025-05-15 22:33:05 +0100

    deployment files

--- backend/index.js
+++ backend/index.js
@@ -16,3 +16,13 @@ app.listen(PORT, () => console.log(`Backend running on port ${PORT}`));
 
 const cors = require("cors");
 app.use(cors());
+
+const client = require("prom-client");
+const collectDefaultMetrics = client.collectDefaultMetrics;
+
+collectDefaultMetrics();
+
+app.get("/metrics", async (req, res) => {
+	res.set("Content-Type", client.register.contentType);
+	res.end(await client.register.metrics());
+});
